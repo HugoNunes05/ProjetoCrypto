da53d71aa53ec1686dbce322f22c9df6a3e3930f
Hugo Nunes <zugofaculdade@gmail.com>
2025-11-19 16:50:27 -0300

    Ajeitando para o deploy

--- crypto-conversor/server.js
+++ crypto-conversor/server.js
@@ -8,4 +8,5 @@ app.use(express.json());
 app.use('/auth', require('./routes/auth'));
 app.use('/crypto', require('./routes/crypto'));
 
-app.listen(3001, () => console.log("Backend rodando porta 3001"));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Backend rodando na porta ${PORT}`));
